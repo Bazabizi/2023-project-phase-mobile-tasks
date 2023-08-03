0501cf9fa9aa3e27e342e7dc7c4e8eb65e60fde2
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2023-08-03 09:56:37 +0300

    Update Task.dart

--- on-boarding/task - 2/Task.dart
+++ on-boarding/task - 2/Task.dart
@@ -1,11 +1,36 @@
 
 class Task {
-  String title;
-  String description;
-  DateTime dueDate;
-  bool status;
+  String _title;
+  String _description;
+  DateTime _dueDate;
+  bool _status;
 
   
   Task({required this.title , required this.description , required this.dueDate , required this.status});
+
+  set title(String title){ 
+    _title = title;
+  }
+
+  set description(String description){
+    _description = description;
+  }
+
+  set dueDate(DateTime dueDate){
+    _dueDate = dueDate;
+  }
+
+  set status(bool status){
+    _status = status;
+  }
+
+  String get title => _title;
+  
+  String get description => _description;
+  
+  DateTime get dueDate => _dueDate;
+  
+  Bool get status => _status;
+  
   
 }
