3c39435b8386a165250bf47f9cd2c6fe2c9266f5
Natnael Tafesse <bazabizi3@gmail.com>
2023-08-15 03:12:25 +0300

    add local data source

--- on-boarding/todo_app/lib/features/todo/data/datasources/task_local_data_source.dart
+++ on-boarding/todo_app/lib/features/todo/data/datasources/task_local_data_source.dart
@@ -1,4 +1,10 @@
+
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/features/todo/data/models/task_model.dart';
+
+import '../../../../core/error/exception.dart';
 abstract class TaskLocalDataSource{
 
   Future<TaskModel> getTask(int id);
@@ -6,10 +12,75 @@ abstract class TaskLocalDataSource{
   Future<void> createTask(TaskModel task);
   Future<void> updateTask(TaskModel task);
   Future<void> deleteTask(int id);
+  Future<TaskModel> getLastTask();
 
-  void cacheTask(TaskModel remoteTask) {}
+  Future<void> cacheTask(TaskModel remoteTask);
 
-  void cacheTasks(List<TaskModel> remoteTasks) {}
+  Future<void> cacheTasks(List<TaskModel> remoteTasks);
 
+
+}
+
+const CACHED_TASK = 'CACHED_TASK';
+
+class TaskLocalDataSourceImpl implements TaskLocalDataSource {
+
+  final SharedPreferences sharedPreferences;
+  TaskLocalDataSourceImpl({required this.sharedPreferences});
+  
+  @override
+  Future<void> cacheTask(TaskModel taskToCache) {
+    return sharedPreferences.setString(
+    CACHED_TASK,
+    json.encode(taskToCache.toJson()),
+  );
+  }
+  
+  @override
+  Future<void> cacheTasks(List<TaskModel> remoteTasks) {
+    // TODO: implement cacheTasks
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> createTask(TaskModel task) {
+    // TODO: implement createTask
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> deleteTask(int id) {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<TaskModel> getTask(int id) {
+    // TODO: implement getTask
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<List<TaskModel>> getTasks() {
+    // TODO: implement getTasks
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> updateTask(TaskModel task) {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<TaskModel> getLastTask() {
+    final jsonString = sharedPreferences.getString(CACHED_TASK);
+    if (jsonString != null) {
+      return Future.value(TaskModel.fromJson(json.decode(jsonString)));
+    } 
+    else {
+      throw CacheException();
+    }
+  }
 
 }
--- on-boarding/todo_app/lib/features/todo/data/datasources/task_remote_data_source.dart
+++ on-boarding/todo_app/lib/features/todo/data/datasources/task_remote_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:http/http.dart' as http;
 import '../models/task_model.dart';
 
 abstract class TaskRemoteDataSource {
@@ -7,4 +8,36 @@ abstract class TaskRemoteDataSource {
   Future<void> updateTask(TaskModel task);
   Future<void> deleteTask(int id);
 
+}
+class TaskRemoteDataSourceImpl implements TaskRemoteDataSource{
+  final http.Client client;
+
+  TaskRemoteDataSourceImpl({required this.client});
+  @override
+  Future<void> createTask(TaskModel task) async {
+}
+
+  @override
+  Future<void> deleteTask(int id) async {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<TaskModel> getTask(int id) async{
+    // TODO: implement getTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<TaskModel>> getTasks() async {
+    // TODO: implement getTasks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateTask(TaskModel task) async {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
 }
